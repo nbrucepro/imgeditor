c40516637dc4d3e6a932eff78192cb607c75efaf
nbrucepro <nezabruce@gmail.com>
2025-12-26 22:11:33 +0800

    first commit

---
+++ menu.py
@@ -0,0 +1,52 @@
+import customtkinter as ctk
+from panels import *
+
+class Menu(ctk.CTkTabview):
+    def __init__(self, parent, pos_vars, color_vars, effect_vars):
+        super().__init__(master=parent)
+        self.grid(row = 0, column = 0, sticky = 'nsew', pady = 10, padx = 10)
+
+        #tabs
+        self.add('Position')
+        self.add('Color')
+        self.add('Effects')
+        self.add('Export')
+
+        #widgets
+        PositionFrame(self.tab('Position'),pos_vars)
+        ColorFrame(self.tab('Color'),color_vars)
+        EffectFrame(self.tab('Effects'),effect_vars)
+
+class PositionFrame(ctk.CTkFrame):
+    def __init__(self, parent, pos_vars ):
+        super().__init__(master = parent, fg_color= 'transparent')
+        self.pack(expand = True, fill = 'both')
+        SliderPanel(self, 'Rotation', pos_vars['rotate'],0,360)
+        SliderPanel(self, 'Zoom',pos_vars['zoom'],0,200)
+        SegmentedPanel(self,'invert', pos_vars['flip'], FLIP_OPTIONS)
+        RevertButton(self, (pos_vars['rotate'],ROTATE_DEFAULT),(pos_vars['zoom'],ZOOM_DEFAULT),pos_vars['flip'],FLIP_OPTIONS[0])
+
+class ColorFrame(ctk.CTkFrame):
+    def __init__(self, parent,color_vars):
+        super().__init__(master = parent, fg_color= 'transparent')
+        self.pack(expand = True, fill = 'both')
+
+        SwitchPanel(self,(color_vars['grayscale'],'B/W'),(color_vars['invert'],'Invert'))
+        SliderPanel(self, 'Brightness', color_vars['brightness'],0,5)
+        SliderPanel(self, 'Vibrance', color_vars['vibrance'],0,5)
+class EffectFrame(ctk.CTkFrame):
+    def __init__(self, parent,effect_vars):
+        super().__init__(master = parent, fg_color= 'transparent')
+        self.pack(expand = True, fill = 'both')
+        DropDownPanel(self, effect_vars['effect'], EFFECT_OPTIONS)
+        SliderPanel(self, 'Blur', effect_vars['blur'],0,30)
+        SliderPanel(self, 'Contrast', effect_vars['contrast'],0,10)
+
+class RevertButton(ctk.CTkButton):
+    def __init__(self,parent,*args):
+        super().__init__(master=parent, text = 'Revert', command=self.revert)
+        self.pack(side = 'bottom',pady =10)
+        self.args =args
+    def revert(self):
+        for var, value in self.args:
+            var.set(value)
---
+++ panels.py
@@ -0,0 +1,48 @@
+import customtkinter as ctk
+from settings import *
+
+class Panel(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master=parent,fg_color=DARK_GREY)
+        self.pack(fill = 'x', pady=4,ipady=8)
+class SliderPanel(Panel):
+    def __init__(self, parent, text, data_var, min_value,max_value):
+        super().__init__(parent = parent)
+
+        #layout
+        self.rowconfigure((0,1), weight=1)
+        self.columnconfigure((0,1), weight=1)
+
+        self.data_var = data_var
+        self.data_var.trace('w',self.update_text)
+
+        ctk.CTkLabel(self,text = text).grid(column = 0, row = 0,sticky= 'W',padx = 5)
+        self.num_label = ctk.CTkLabel(self, text = data_var.get())
+        self.num_label.grid(column = 1, row = 0, sticky = 'E', padx = 5)
+        ctk.CTkSlider(self, 
+                      fg_color=SLIDER_BG, 
+                      from_= min_value,
+                      to = max_value,
+                      command=self.update_text,
+                      variable=data_var).grid(row = 1, column = 0, columnspan = 2, sticky = 'ew', padx= 5, pady = 5 )
+    def update_text(self, *args):
+        self.num_label.configure(text = f'{round(value,2)}')
+
+class SegmentedPanel(Panel):
+    def __init__(self, parent, text, data_var, options ):
+        super().__init__(parent = parent)
+        ctk.CTkLabel(self,text=text).pack()
+        ctk.CTkSegmentedButton(self,variable=data_var,values=options).pack(expand = True, fill = 'both',padx=4, pady = 4)
+
+class SwitchPanel(Panel):
+    def __init__(self, parent, *args):
+        super().__init__(parent = parent)
+        for var,text in args:
+            switch = ctk.CTkSwitch(self, text=text,variable=var,button_color=BLUE,fg_color=SLIDER_BG)
+            switch.pack(side = 'left',expand = True,fill='both',padx=5, pady= 5)
+
+class DropDownPanel(ctk.CTkOptionMenu):
+    def __init__(self,parent,data_var,options):
+        super().__init__(master=parent,values=options,fg_color=DARK_GREY,button_color=DROPDOWN_MAIN_COLOR,
+                         button_hover_color=DROPDOWN_HOVER_COLOR,dropdown_fg_color=DROPDOWN_MENU_COLOR,variable=data_var)
+        self.pack(fill = 'x', pady = 4)
